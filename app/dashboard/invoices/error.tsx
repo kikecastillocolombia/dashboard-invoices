f0689884fd3deb0e822c4ca58309fedcf3d1b05e
kikecastillocolombia <enriquecastiyosoy@gmail.com>
2024-10-21 01:59:11 -0500

    crud invoices

---
+++ app/dashboard/invoices/error.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Opcionalmente, registra el error en un servicio de informes de errores
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">¡Algo salió mal!</h2>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Intenta recuperarte volviendo a renderizar la ruta de facturas
+          () => reset()
+        }
+      >
+        Intentar de nuevo
+      </button>
+    </main>
+  );
+}
